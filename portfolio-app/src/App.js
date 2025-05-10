805b325694f8970d51699848884fffa793a9400e
SriVarshan242 <varshansri0123@gmail.com>
2025-05-11 00:21:32 +0530

    changes in

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const basename = process.env.NODE_ENV === 'production' ? '/Portfolio' : '/';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
