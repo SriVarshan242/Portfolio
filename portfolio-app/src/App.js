2e6836967ff2c7441299a1e556b89dcb1f8f5276
SriVarshan242 <varshansri0123@gmail.com>
2025-05-11 00:23:16 +0530

    after imports

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const basename = process.env.NODE_ENV === 'production' ? '/Portfolio' : '/';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+const basename = process.env.NODE_ENV === 'production' ? '/Portfolio' : '/';
 
 function App() {
  return (
